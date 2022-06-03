bd26c55f914a1ea090eda8ce44b6b3019e2d6d68
Owen Hathaway <owen@prsdata.com>
2022-06-03 12:48:33 -0600

    fixed some merge conflicts

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <TheHeader></TheHeader>
   <!-- render posts -->
   <NuxtPage />
@@ -20,42 +19,4 @@ useHead({
     { name: 'og:title', content: `Cooking with the Hathaways - ${route.meta.title}` }
   ]
 })
-=======
-  <div>
-    <!-- render posts -->
-    <ul>
-      <li v-for="post in data.posts">{{ post.fields.title}}</li>
-    </ul>
-  </div>
-</template>
-
-<script setup>
-import * as contentful from '~/plugins/contentful.js'
-
-const client = contentful.default.createClient()
-
-const { data } = await useAsyncData( 'person', ()  => {
-  // console.debug('env keys from app.vue: ', Object.keys(env))
-  return Promise.all([
-    // fetch the owner of the blog
-    client.getEntries({
-      'sys.id': process.env.CTF_PERSON_ID
-    }),
-    // fetch all blog posts sorted by creation date
-    client.getEntries({
-      content_type: process.env.CTF_BLOG_POST_TYPE_ID,
-      order: '-sys.createdAt'
-    })
-  ]).then(([entries, posts]) => {
-    console.debug('entries: ', entries)
-    // return data
-    return {
-      person: entries.items[0],
-      posts: posts.items
-    }
-  }).catch(console.error)
-})
-
-console.debug('data: ', JSON.stringify(data, null, 2))
->>>>>>> ed4ecf0 (initial commit after a bit of stubbing things out)
 </script>
