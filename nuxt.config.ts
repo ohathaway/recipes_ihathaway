84b0e164027a03a3fe7d9de382472df9ffbfadd2
Owen Hathaway <owen@prsdata.com>
2022-06-03 12:39:52 -0600

    resetting after merge with remote

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+<<<<<<< HEAD
   modules: [
     '@nuxtjs/apollo'
   ],
@@ -28,4 +29,14 @@ export default defineNuxtConfig({
       default: '@/plugins/graphql/config',
     }
   }
+=======
+/*
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: ctfConfig.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
+  }
+*/
+>>>>>>> ed4ecf0 (initial commit after a bit of stubbing things out)
 })
