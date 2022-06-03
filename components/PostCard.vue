15af97e7bdd02e7ef4d8d9e387cb38020083f3b1
Owen Hathaway <owen@prsdata.com>
2022-06-03 13:36:44 -0600

    basic layout of recipe listing

---
+++ components/PostCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="card">
+    <div class="card-title">
+      <h2>This a Recipe</h2>
+    </div>
+    <div class="card-body">
+      <p>
+        And this is the content
+      </p>
+    </div>
+  </article>
+</template>
